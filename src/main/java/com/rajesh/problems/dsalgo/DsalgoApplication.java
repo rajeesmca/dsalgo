f98c786d959a065512c5aaece57875bdf602ac16
Rajesh <rajeesmca@gmail.com>
2020-10-12 12:07:17 +0530

    Initial commit

---
+++ src/main/java/com/rajesh/problems/dsalgo/DsalgoApplication.java
@@ -0,0 +1,13 @@
+package com.rajesh.problems.dsalgo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsalgoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsalgoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rajesh/problems/dsalgo/DsalgoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rajesh.problems.dsalgo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsalgoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
